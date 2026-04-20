491ecd58e039b7b26ce17ebe47ce6ebbb9744850
Adesh <adeshsonpure@gmail.com>
2026-04-20 16:12:56 +0530

    home page done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="navbar">
         <Link href="/">
         <div className="flex items-center gap-2.5 cursor-pointer">
-            <Image src ="/images/logo.svg"
+            <Image src ="/images/logo.png"
              alt ="logo" 
              width ={46} 
              height={44} />
